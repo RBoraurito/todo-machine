0ae1851eddff860b51ac134e33a1a4ae4bb4f182
RBoraurito <r.boraure@kation.com.do>
2021-10-19 20:52:35 -0400

    :tada: Initialize proyect

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        <TodoItem />
+      </TodoList>
+      <CreateTodoButton />
+    </>
+  )
+}
+
+export default App
